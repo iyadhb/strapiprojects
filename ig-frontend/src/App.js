be972f018b1b2cba939a81467dc724388bb9eac8
Iyad <iyadhb@gmail.com>
2020-12-31 19:33:08 +0000

    list.map

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -2,22 +2,43 @@ import './App.css';
 
 import Post from './components/Post'
 
-const post = {
-  likes: 20,
-  description: "This is my post",
-  image: {
-    url: "/uploads/pexels_snapwire_34950_df1d2d4f18.jpg"
+
+const posts = [
+  {
+    likes: 20,
+    description: "This is my post",
+    image: {
+      url: "/uploads/pexels_snapwire_34950_df1d2d4f18.jpg"
+          }
+    },
+    {
+      likes: 33,
+      description: "Second post descriptin",
+      image: {
+        url: "/uploads/pexels_snapwire_34950_df1d2d4f18.jpg"
+          }
+    },
+    {
+    likes: 50,
+    description: "Third post inline",
+    image: {
+      url: "/uploads/pexels_snapwire_34950_df1d2d4f18.jpg"
+    }
   }
-}
+]
+
 
 function App() {
   return (
     <div className="App">
-    <Post
+    {[posts.map(post =>(
+       <Post
       likes={post.likes}
       description={post.description}
       url={post.image && post.image.url}
     />
+    ))]}
+   
     </div>
   );
 }
