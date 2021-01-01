9c0bb52ed2e1aab383ce18b96f144dcd464e30fb
Iyad <iyadhb@gmail.com>
2021-01-01 21:06:17 +0000

    added image upload to the create form

--- ig-frontend/src/pages/Create.js
+++ ig-frontend/src/pages/Create.js
@@ -3,17 +3,20 @@ import React, {useState} from 'react'
 const Ceate = () => {
     
     const [description, setDescription] = useState('')
+    const [file, setFile] = useState(null)
+
+    console.log("file", file)
 
     const handleSubmit = async (event) => {
         event.preventDefault()
+
+        const formData = new FormData()
+        formData.append('data', JSON.stringify({description}))
+        formData.append('files.image', file)
+
         const response = await fetch('http://localhost:1337/posts',{
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                description
-            })
+            body: formData
         })
         const data = await response.json()
         console.log("data", data)
@@ -22,12 +25,18 @@ const Ceate = () => {
         <div className="Create">
             <h2>Create</h2>
             <form onSubmit={handleSubmit}>
-            <input 
-            placeholder="Description" 
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-            />
-            <button>Submit</button>
+                <input 
+                    placeholder="Description" 
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                />
+                <input
+                    type="file"
+                    placeholder="Add a File"
+                    onChange={(event) => setFile(event.target.files[0])}
+
+                />
+                <button>Submit</button>
             
             </form>
         </div>
