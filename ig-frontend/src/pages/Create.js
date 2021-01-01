a196fe2bbabe2a808f361dc4a37dd3a1e372d3ed
Iyad <iyadhb@gmail.com>
2021-01-01 20:52:57 +0000

    setup the Create Post page

--- ig-frontend/src/pages/Create.js
+++ ig-frontend/src/pages/Create.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Ceate = () => {
+    
+    const [description, setDescription] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const response = await fetch('http://localhost:1337/posts',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                description
+            })
+        })
+        const data = await response.json()
+        console.log("data", data)
+    }
     return (
         <div className="Create">
-            Create
+            <h2>Create</h2>
+            <form onSubmit={handleSubmit}>
+            <input 
+            placeholder="Description" 
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            />
+            <button>Submit</button>
+            
+            </form>
         </div>
     )
 }
